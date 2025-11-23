69d3c5992e63cc91b6bd0ba4b38a9d1239e453b2
DevChico <eduardob1741@gmail.com>
2025-11-23 14:43:32 -0300

    Add files via upload

---
+++ Exercício 8 – Conversor de Medidas.py
@@ -0,0 +1,3 @@
+metros = float(input('Digite uma distância em metros: '))
+print(f'A medida de {metros}m corresponde a:')
+print(f'Em Quilometro: {metros / 1000}\nEm Hectômetro: {metros / 100}\nEm Decâmetro: {metros / 10}\nEm Decímetro: {metros * 10}\nEm Centímetro: {metros * 100}\nEm Milímetro: {metros * 1000}')
