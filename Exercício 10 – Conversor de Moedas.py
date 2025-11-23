e61edeefb003873956ad1898c4582594d34c5152
DevChico <eduardob1741@gmail.com>
2025-11-23 14:44:06 -0300

    Add files via upload

---
+++ Exercício 10 – Conversor de Moedas.py
@@ -0,0 +1,3 @@
+dinheiro_wallet = float(input('Quanto dinheiro você tem na carteira: R$'))
+print(f'Com {dinheiro_wallet} você pode comprar {dinheiro_wallet / 5.40:.2f}USD')
+print(f'Com {dinheiro_wallet} você pode comprar {dinheiro_wallet / 6.14:.2f}EUR')
