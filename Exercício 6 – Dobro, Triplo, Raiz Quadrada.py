bfc22bac6d58a9a88944ff60b3f0b32457c21af3
DevChico <eduardob1741@gmail.com>
2026-01-11 19:30:34 -0300

    Update Exercício 6 – Dobro, Triplo, Raiz Quadrada.py

--- Exercício 6 – Dobro, Triplo, Raiz Quadrada.py
+++ Exercício 6 – Dobro, Triplo, Raiz Quadrada.py
@@ -1,2 +1,2 @@
 numero = int(input('Digite um número: '))
-print(f'O seu Dobro é {numero * 2}\nO seu Triplo é {numero * 3}\nA sua Raiz quadrada é {numero ** (1/2):.2f}')
+print(f'O seu Dobro é \033[32m{numero * 2}\033[m\nO seu Triplo é \033[32m{numero * 3}\033[m\nA sua Raiz quadrada é \033[33m{numero ** (1/2):.2f}\033[m')
