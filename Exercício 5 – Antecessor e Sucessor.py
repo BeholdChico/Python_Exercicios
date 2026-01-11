64cfbeb8442ccbe4a9a79dd7bc5d618856d0017e
DevChico <eduardob1741@gmail.com>
2026-01-11 19:27:37 -0300

    Update Exercício 5 – Antecessor e Sucessor.py

--- Exercício 5 – Antecessor e Sucessor.py
+++ Exercício 5 – Antecessor e Sucessor.py
@@ -1,2 +1,2 @@
 numero = int(input('Digite um número: '))
-print(f'Analisando o valor, {numero}... Seu Antercessor é: {numero - 1} e seu Sucessor é {numero + 1}.')
+print(f'Analisando o valor, {numero}... Seu Antercessor é: \033[31m{numero - 1}\033[m e seu Sucessor é \033[32m{numero + 1}\033[m.')
