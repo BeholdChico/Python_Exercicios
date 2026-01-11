c32f63d73dcad7daf4a2e02e0485f65531c36193
DevChico <eduardob1741@gmail.com>
2026-01-11 19:13:11 -0300

    Update Exercício 3 – Somando dois números.py
    Added color formatting to the output for better visibility.

--- Exercício 3 – Somando dois números.py
+++ Exercício 3 – Somando dois números.py
@@ -1,3 +1,3 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
-print(f'A soma de {n1} + {n2} é igual a {n1 + n2}.')
+print(f'A soma de \033[33m{n1}\033[m + \033[34m{n2}\033[m é igual a \033[32m{n1 + n2}\033[m.')
