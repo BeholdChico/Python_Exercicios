59fcb651901b137248fab82ce7074c4b2d6378c6
DevChico <eduardob1741@gmail.com>
2026-01-12 21:48:19 -0300

    Update Exercício 7 – Média Aritmética.py

--- Exercício 7 – Média Aritmética.py
+++ Exercício 7 – Média Aritmética.py
@@ -1,3 +1,3 @@
 primeira_nota = float(input('Digite a primeira nota do aluno: '))
 segunda_nota = float(input('Digite a segunda nota do aluno: '))
-print(f'A media entre {primeira_nota} e {segunda_nota} é igual a {(primeira_nota + segunda_nota) / 2:.1f}')
+print(f'A media entre {primeira_nota} e {segunda_nota} é igual a \033[32m{(primeira_nota + segunda_nota) / 2:.1f}\033[m')
